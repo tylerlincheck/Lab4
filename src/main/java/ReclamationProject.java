a89b7950b3b10ca055e22cf191f7867b59545569
tyler <tyler@DESKTOP-D62JGHO>
2017-09-26 12:47:20 -0500

    Lab 4

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,27 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+/** */
+public class ReclamationProject {
+    /** */
+    static String doit(final String firstInput, final String secondInput) {
+        /** */
+        String shortInput = firstInput;
+        String longInput = secondInput;
+        if (firstInput.length() > secondInput.length()) {
+            shortInput = secondInput;
+            longInput = firstInput;
+        }
+        String output = "";
+        for (int i = 0; i < shortInput.length(); i++) {
+            for (int j = shortInput.length() - i; j > 0; j--) {
+                for (int k = 0; k < longInput.length() - j; k++) {
+                    if (shortInput.regionMatches(i, longInput, k, j) && j > output.length()) {
+                        output = shortInput.substring(i, i + j);
+                    }
+                        }
+                }
+        }
+        return output;
+        }
 }
